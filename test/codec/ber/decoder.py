51db808b6da9d9cf688ed02147adff9a9580fa88
elie <elie>
2012-05-27 09:30:01 +0000

    fix to leading 0x80 octet handling in CER/DER ObjectIdentifier decoder

--- test/codec/ber/decoder.py
+++ test/codec/ber/decoder.py
@@ -140,6 +140,11 @@ class ObjectIdentifierDecoderTestCase(unittest.TestCase):
             ints2octs((6, 7, 43, 6, 143, 255, 255, 255, 127))
             ) == ((1, 3, 6, 4294967295), null)
 
+    def testEdges4(self):
+        assert decoder.decode(
+            ints2octs((6, 5, 85, 4, 128, 129, 0))
+            ) == ((2, 5, 4, 128), null)
+
 class SequenceDecoderTestCase(unittest.TestCase):
     def setUp(self):
         self.s = univ.Sequence(componentType=namedtype.NamedTypes(
