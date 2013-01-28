8ce8211e2a76b1eedcecd6eba7787516b8c9c169
elie <elie>
2013-01-28 08:13:46 +0000

    some more CHOICE codec test cases added

--- test/codec/ber/encoder.py
+++ test/codec/ber/encoder.py
@@ -279,7 +279,8 @@ class ChoiceEncoderTestCase(unittest.TestCase):
     def setUp(self):
         self.s = univ.Choice(componentType=namedtype.NamedTypes(
             namedtype.NamedType('place-holder', univ.Null('')),
-            namedtype.NamedType('number', univ.Integer(0))
+            namedtype.NamedType('number', univ.Integer(0)),
+            namedtype.NamedType('string', univ.OctetString())
             ))
 
     def testEmpty(self):
@@ -296,10 +297,21 @@ class ChoiceEncoderTestCase(unittest.TestCase):
 
     def testTagged(self):
         s = self.s.subtype(
-            explicitTag=tag.Tag(tag.tagClassContext, tag.tagFormatSimple, 4)
-            )
+            explicitTag=tag.Tag(tag.tagClassContext,tag.tagFormatConstructed,4)
+        )
         s.setComponentByPosition(0, univ.Null(''))
         assert encoder.encode(s) == ints2octs((164, 2, 5, 0))
+
+    def testUndefLength(self):
+        self.s.setComponentByPosition(2, univ.OctetString('abcdefgh'))
+        assert encoder.encode(self.s, defMode=False, maxChunkSize=3) == ints2octs((36, 128, 4, 3, 97, 98, 99, 4, 3, 100, 101, 102, 4, 2, 103, 104, 0, 0))
+
+    def testTaggedUndefLength(self):
+        s = self.s.subtype(
+            explicitTag=tag.Tag(tag.tagClassContext,tag.tagFormatConstructed,4)
+        )
+        s.setComponentByPosition(2, univ.OctetString('abcdefgh'))
+        assert encoder.encode(s, defMode=False, maxChunkSize=3) == ints2octs((164, 128, 36, 128, 4, 3, 97, 98, 99, 4, 3, 100, 101, 102, 4, 2, 103, 104, 0, 0, 0, 0))
 
 class AnyEncoderTestCase(unittest.TestCase):
     def setUp(self):
